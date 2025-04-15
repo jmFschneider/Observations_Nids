0872a1952ba4e2aed2617f63a6d316500a8dec6e
schneider <schneider.jm@free.fr>
2025-04-15 23:18:26 +0200

    Mise en place du controle des outils pour le debuggage et ne pas les avoir en prod

--- Observations_Nids/settings.py
+++ Observations_Nids/settings.py
@@ -44,7 +44,8 @@ LOGIN_REDIRECT_URL = '/'
 SESSION_COOKIE_AGE = 3600
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Déconnexion si l’utilisateur ferme son navigateur
 
-ALLOWED_HOSTS: list[str] = []
+A
+ALLOWED_HOSTS: list[str] = ['*', 'observation-nids.meteo-poelley50.fr', 'localhost', '127.0.0.1']
 
 
 # Application definition
